f0e250103c8342ca67acdd886e0a92b5040fcdf4
Rasheed Abdul-Aziz <rabdulaziz@pivotal.io>
2019-10-16 10:30:07 -0400

    Added credalert to base
    - this will definitely break on unix at the moment

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
 RCRC="${HOME}/workspace/isv-ci-workstation-config/dotfiles/rcrc" rcup -v
+
+
+# Install credalert
+wget https://s3.amazonaws.com/cred-alert/cli/current-release/cred-alert-cli_darwin
+chmod +x cred-alert-cli_darwin
+mv cred-alert-cli_darwin /usr/local/bin/cred-alert-cli
+cred-alert-cli update
+
+if [ ! -d$HOME/workspace/git-hooks-core ]; then
+  git clone https://github.com/pivotal-cf/git-hooks-core.git $HOME/workspace/git-hooks-core
+fi
